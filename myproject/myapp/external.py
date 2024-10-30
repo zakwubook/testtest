710ed54f06027f3d28409d2017680a796eb9a890
zakwubook <deborahparker9044653@gmail.com>
2024-10-31 00:07:26 +0100

    Add files via upload

--- myproject/myapp/external.py
+++ myproject/myapp/external.py
@@ -1,21 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-
-
-from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
 def check_credentials(username, password):
     options = Options()
@@ -41,12 +30,15 @@ def check_credentials(username, password):
         # Нажимаем кнопку входа
         login_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
         login_button.click()
-        print(driver.page_source)
-        # Проверяем, произошла ли ошибка аутентификации
-        if "Authentication Failed" in driver.page_source:
-            return False
-        else:
+        
+        # Ждем, пока URL изменится
+        wait.until(EC.url_changes(driver.current_url))
+
+        # Проверяем, на какую страницу мы перенаправлены
+        if driver.current_url == "https://wubook.net/zks/manage/dboard/":
             return True
+        else:
+            return False
     except Exception as e:
         print(f"An error occurred: {e}")
         return False
